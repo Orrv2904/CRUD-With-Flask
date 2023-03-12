7cccdd7547e821575bd6789f7caf9e9a7e19c181
Orrv2904 <Omarrv2904@gmail.com>
2023-03-11 18:32:25 -0600

    Redireccion de login iniciado a medias :construction:

--- helpers.py
+++ helpers.py
@@ -2,7 +2,7 @@ import os
 import requests
 import urllib.parse
 
-from flask import redirect, render_template, request, session
+from flask import redirect, render_template, request, session, url_for
 from functools import wraps
 
 
@@ -19,10 +19,22 @@ def login_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
+'''
 def redirecthome(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("user_id") is not None:
             return redirect("/flights")
+        return f(*args, **kwargs)
+    return decorated_function
+'''
+
+def redirecthome(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is not None:
+            if request.referrer and "/login" in request.referrer:
+                return redirect("/flights")
+            return redirect("/")
         return f(*args, **kwargs)
     return decorated_function
